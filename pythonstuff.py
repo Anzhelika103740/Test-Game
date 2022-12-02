82d1e31a5bc96a502ca3d9a6493e7b430123b553
anzhelika2022 <anzhelikatosheva@ua.pt>
2022-12-02 00:30:14 +0000

    guess

--- pythonstuff.py
+++ pythonstuff.py
@@ -4,3 +4,15 @@ number = random.randint(1,10)
 player_name = input("Hello, What's your name?")
 number_of_guesses = 0
 print('okay! '+ player_name+ ' I am Guessing a number between 1 and 10:')
+
+while number_of_guesses < 5:
+    guess = int(input())
+    number_of_guesses += 1
+    if guess < number:
+        print('Your guess is too low')
+    if guess > number:
+        print("Your guess is too high")
+    if guess == number: 
+        print("Correct")
+        break
+        
