c13f6927f20611ad6a5dda7dd5b301cca3715195
anzhelika2022 <anzhelikatosheva@ua.pt>
2022-12-02 00:28:20 +0000

    guessing

--- pythonstuff.py
+++ pythonstuff.py
@@ -1,5 +1,6 @@
-test = 3
+import random
 
-while test == True:
-    print("Hello World")
- 
+number = random.randint(1,10)
+player_name = input("Hello, What's your name?")
+number_of_guesses = 0
+print('okay! '+ player_name+ ' I am Guessing a number between 1 and 10:')
